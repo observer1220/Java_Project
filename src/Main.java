161f2e2fe9dc4db68abeeda63991bdba531ee7f7
observer1220 <observer1220@gmail.com>
2024-08-13 13:58:03 +0800

    20240813-2

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,14 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
+import Shapes.Circle;
 
 public class Main {
-    public int multiplier(int a, int b) {
-        return a * b;
-    }
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(10);
+        Circle circle2 = new Circle(5);
+        Circle circle3 = new Circle();
 
-    public void main(String[] args) {
-        ArrayList<String> friends = new ArrayList<>();
-        ArrayList<Integer> scores = new ArrayList<>();
-
-        friends.add("Mike");
-        friends.add("Wilson");
-        friends.add("Grace");
-        friends.add("Abigail");
-//        System.out.println(friends);
-//        System.out.println(friends.get(2));
-
-        Collections.sort(friends);
-//        System.out.println(friends);
-//        System.out.println(friends.size());
-
-//        friends.clear();
-//        System.out.println(friends);
-
-        scores.add(99);
-//        System.out.println(scores.getFirst());
-
-        int[][] my2dArray = new int[4][3];
-        my2dArray[2][0] = 10;
-//        System.out.println(Arrays.deepToString(my2dArray));
-//        for (int i = 0; i < friends.size(); i++) {
-//            System.out.println(i + 1 + ". " + friends.get(i));
-//        }
-//
-//        for (String name : friends) {
-//            System.out.println(name);
-//        }
-
-//        int counter = 0;
-//        while (counter < 5) {
-//            System.out.println(counter);
-//            counter++;
-//        }
-
-//        for (int i = 0; i < 10; i++) {
-//            for (int j = 3; j > 0; j--) {
-//                System.out.println(i + ", " + j);
-//            }
-//        }
-
-        for (int i = 1; i <= 10; i++) {
-            if (i == 5) {
-                System.out.println("停止迴圈，當 i = " + i);
-                break; // Exit the loop when i equals 5
-            }
-
-            if (i % 2 == 0) {
-                System.out.println("跳過偶數: " + i);
-                continue; // Skip the rest of the loop body for even numbers
-            }
-
-            System.out.println("目前數字: " + i);
-        }
-
-        System.out.println(multiplier(2, 2) + multiplier(3, 3));
+        System.out.println(circle1.getArea());
+        System.out.println(circle2.getArea());
+        System.out.println(circle3.getArea());
+        System.out.println(circle1.getRadius());
     }
 }
--- src/Shapes/Circle.java
+++ src/Shapes/Circle.java
@@ -1,4 +1,23 @@
 package Shapes;
 
 public class Circle {
+    private double radius;
+    private static final double PI = 3.14;
+
+    public Circle() {
+        this(1.0);
+    }
+
+    // Circle Class Constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return PI * Math.pow(this.radius, 2);
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
 }
